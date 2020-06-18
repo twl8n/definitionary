ff718b76edff653a0ed1551d4c03938f33536223
slaudeman <44531271+slaudeman@users.noreply.github.com>
2020-06-18 17:18:26 -0400

    SVG Update
    - nonSVG soln
    + image underlay for SVG
    + scaling for SVG container based on image width/dim
    
    ! Consider looking at responsive image sizing and how that affects the svg elements

--- javascriptRef.js
+++ javascriptRef.js
@@ -1,112 +1,78 @@
-/* const myCanvas = document.getElementById("my-canvas")
-const myContext = myCanvas.getContext("2d") */
 
-/* onload image draw (changed to canvas background css)
-const img = new Image()
-img.src = "./img/r850r.jpg"
-img.onload = () => {
-	myContext.drawImage(img, 0, 0)
-} */
-
-function drawTextBox(e) {
-	// Draw text box at click location.
-	const rect = myCanvas.getBoundingClientRect()
-	var x = e.clientX - rect.left;
-	var y = e.clientY - rect.top;
-	
-	console.log("Int: x: " + x + " y: " + y)
-	
-	var size = prompt("Enter the size of the box as x,y");
-	console.log(size);
-	
-	var res = size.split(",");
-	var xSize = parseInt(res[0]);
-	var ySize = parseInt(res[1]);
-	
-	myContext.beginPath();
-	myContext.fillRect(x,y,xSize,ySize);
-	console.log(x,y,xSize,ySize);
-	myContext.stroke();
-	
-}
-
-function drawTextBox2(e) {
-	// Draw text box at click location. Box sizes to text automatically.
-	const rect = myCanvas.getBoundingClientRect()
-	var x = e.clientX - rect.left;
-	var y = e.clientY - rect.top;
-	
-	console.log("Int: x: " + x + " y: " + y);
-	
-	var txt = prompt("Enter text");
-	
-
-	var font = '12pt Arial';
-	myContext.font = font;
-	myContext.textBaseline = 'top';
-	var width = myContext.measureText(txt).width;
-	var height = myContext.measureText(txt).height;
-	
-	myContext.fillStyle = '#f50';
-	myContext.fillRect(x,y,width + 10,parseInt(font,10)+6);
-	
-	myContext.fillStyle = '#000';
-	
-	myContext.fillText(txt,x + 5,y + 2);
-	
-	
-}
-
-
-	
-
-
-function drawSVGTextBox(e) {
+function drawSVGTextBox(e,n) {
 	//draw SVG text box at cursor location
 	
-	const rect = svg1.getBoundingClientRect();
-	
-	var x = e.clientX - rect.left;
-	var y = e.clientY - rect.top;
-	console.log("x: " + x + " y: " + y);
-	
-	var txt = prompt("Enter text");
-	var t = document.createTextNode(txt);
-	
-	element = document.createElementNS("http://www.w3.org/2000/svg", "text");
-	element.setAttributeNS(null, 'x', x);
-	element.setAttributeNS(null, 'y', y);
+	if (e.target.getAttribute('id') == "svg1"){
+		console.log("targeted " + e.target.getAttribute('id'));
+		
+		const rect = svg1.getBoundingClientRect();
+		
+		var x = e.clientX - rect.left;
+		var y = e.clientY - rect.top;
+		console.log("x: " + x + " y: " + y);
+		
+		var txt = prompt("Enter text");
+		var t = document.createTextNode(txt);
+		
+		var stringID = x + "-" + y;
+		
+		element = document.createElementNS("http://www.w3.org/2000/svg", "text");
+		element.setAttributeNS(null, 'x', x);
+		element.setAttributeNS(null, 'y', y);
+		element.setAttributeNS(null, 'id',stringID);
 
-	
-	element.appendChild(t);
-	
-	console.log(element);
+		
+		element.appendChild(t);
+		
+		console.log(element);
 
-	svg1.appendChild(element);
-	
-	SVGRect = element.getBBox();
-	var rect1 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-	rect1.setAttributeNS(null,"x",SVGRect.x);
-	rect1.setAttributeNS(null,"y",SVGRect.y);
-	rect1.setAttributeNS(null,"width",SVGRect.width);
-	rect1.setAttributeNS(null,"height",SVGRect.height);
-	rect1.setAttributeNS(null,"fill","cyan");
-	rect1.setAttributeNS(null,"onclick","remove");
-	
-	textElem = svg1.getElementById(element);
-	console.log(textElem);
+		svg1.appendChild(element);
+		
+		SVGRect = element.getBBox();
+		var rect1 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+		rect1.setAttributeNS(null,"x",SVGRect.x);
+		rect1.setAttributeNS(null,"y",SVGRect.y);
+		rect1.setAttributeNS(null,"width",SVGRect.width);
+		rect1.setAttributeNS(null,"height",SVGRect.height);
+		rect1.setAttributeNS(null,"fill","cyan");
+		rect1.setAttributeNS(null,"id","rect" + stringID);
+		
+		console.log(rect1);
 
+		
+		svg1.appendChild(rect1);
+		svg1.appendChild(element);	
 	
-	svg1.appendChild(rect1);
-	console.log(rect1);
-	svg1.appendChild(element);	
+	}
+	else {
+		console.log(e.target);
+		
+		id = e.target.getAttribute('id')
+		
+		console.log(id);
+		e.target.remove();
+		svg1.getElementById('rect'+ id).remove();
+		
+	}
 	
 }
 
-//svg1.addEventListener("click",drawSVGTextBox,false);
+function searchByID(id) {
+	//Query DB/JSON for coords
+	if (svg1.getElementById(id) != null) {
+		var coord = id.split("-");
+		var xCoord = coord[0];
+		var yCoord = coord[1];
+		//can query JSON for x,y coord, load from JSON
+		//return "exists", query db and draw?
+	}
+	else {
+		//do something else
+	}
+}
+svg1.addEventListener("click", drawSVGTextBox, false);
 
 
-
-myCanvas.addEventListener("click", drawTextBox2, false);
+//myCanvas.addEventListener("click", drawTextBox2, false);
 
 
