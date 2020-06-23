46f3f2a4c7d559363662e5a2bb3e2f48b3811fc8
Tom Laudeman <tlaudeman@roomkey.com>
2020-06-22 21:31:15 -0400

    Three languages with definitions in all three

--- schema.sql
+++ schema.sql
@@ -8,22 +8,25 @@ create table defini (
 
 -- For the text describing a language dtext.id = dtext.lang
 create table dtext (
-        did int, -- fk to defini.id
+        did int not null, -- fk to defini.id
         lang int, -- fk to defini.id of the language of this definition
-        phrase text -- definition text in the language
+        phrase text, -- definition text in the language
+        foreign key  (did) references defini (id),
+        foreign key (lang) references defini (id)
         );
 
 create table topword (
         did int, -- fk to definition id
         lang int, -- fk to lang id
-        myword text -- single word or phrase
+        myword text, -- single word or phrase
+        foreign key  (did) references defini (id),
+        foreign key (lang) references defini (id)
         );
 
 -- Create a covering index, which is apparently a bit more efficient than an index.
 -- https://www.sqlite.org/queryplanner.html#covidx
 
 create unique index idx1 on topword (did, lang, myword);
-
 
 insert into defini (is_language) values (1);
 insert into dtext (did,lang,phrase) values ((select id from defini where rowid=last_insert_rowid()),(select id from defini where rowid=last_insert_rowid()),'English language');
@@ -44,27 +47,29 @@ insert into topword (did,lang,myword) values (3,1,'Spanish');
 
 
 insert into dtext (did,lang,phrase) values   (1,2,"l'Anglais");
-insert into topword (did,lang,phrase) values (1,2,'Anglais');
+insert into topword (did,lang,myword) values (1,2,'Anglais');
 insert into dtext (did,lang,phrase) values   (3,2,"l'Espagnol");
 insert into topword (did,lang,myword) values  (3,2,"Espagnol");
 
 insert into dtext (did,lang,phrase) values   (1,3,"idioma en Inglés");
-insert into topword (did,lang,phrase) values (1,3,'Inglés');
+insert into topword (did,lang,myword) values (1,3,'Inglés');
 insert into dtext (did,lang,phrase) values   (2,3,"idioma francés");
 insert into topword (did,lang,myword) values  (2,3,"Francés");
 
 
 .headers on
 .mode column
-select defini.id,topword.myword
-from defini, dtext, topword
+select defini.id,defini.is_language,zz.myword,dtext.lang,(select myword from topword xx where xx.did=dtext.lang) as lang
+from defini, dtext, topword zz
 where
     defini.is_language=1
     and
-    defini.id=dtext.lang
+    dtext.did = defini.id
     and
-    defini.id = topword.did;
-
+    defini.id = zz.did
+    and
+    dtext.lang = zz.lang
+order by defini.id;
 
 select defini.id from defini, dtext, topword
 where
